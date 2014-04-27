747550ff9adc856f77e1a9a1eb07ef3c92e4ac51
Adam Davey <adavey@adams-air.home>
2014-04-27 15:28:07 -0400

    Modified original matrix inverse caching program. Passes several tests (inv%*%a ~= I, agrees with output of solve for same matrices).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Gets cached inverse or else calls solver and caches results
+makeCacheMatrix <- function(x = numeric()) {
+   invx <- NULL
+   set <- function(y) {
+     x <<- y
+     invx <<- NULL
+   }
+   get <- function() x
+   setinv <- function(solve) invx <<- solve
+   getinv <- function () invx
+   list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Calculates inverse of square matrix x
+## If cached not available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invx <- x$getinv()
+  if(!is.null(invx)) {
+    message("getting cached data")
+    return(invx)
+  }
+  data <- x$get()
+  invx <- solve(data, ...)
+  x$setinv(invx)
+  invx
 }
